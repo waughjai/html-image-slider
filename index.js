c4c24ec9c0ad10b45df26d59389984ea25eb87d4
jjwmezun <jjwmezun@gmail.com>
2018-12-27 16:32:04 -0800

    Complete slider

--- index.js
+++ index.js
@@ -2,8 +2,21 @@
 {
 	const ItemClass = 'waj-image-slider-item';
 	const Items = document.getElementsByClassName( ItemClass );
+	const SliderStates =
+	{
+		Zooming: 0,
+		Fading: 1
+	};
+	const OriginalItemWidth = 100;
+	const OriginalItemWidthSpeed = 0.5;
+	const ItemWidthDeceleration = 1.008;
+	const ItemFadeSpeed = 5000;
+
 	let CurrentItem = 0;
+	let CurrentSliderState = SliderStates.Zooming;
 	let PreviousTimestamp = 0;
+	let CurrentItemWidth = OriginalItemWidth;
+	let CurrentItemWidthSpeed = OriginalItemWidthSpeed;
 
 	const SetImagePriority = function()
 	{
@@ -32,18 +45,50 @@
 
 	const Update = function( Timestamp )
 	{
-		if ( Timestamp - PreviousTimestamp >= 1200 )
+		switch ( CurrentSliderState )
 		{
-			Items[ CurrentItem ].style.opacity = 1000 / ( Timestamp - PreviousTimestamp - 1200 );
-			if ( Items[ CurrentItem ].style.opacity <= 0 )
+			case ( SliderStates.Zooming ):
 			{
-				PreviousTimestamp = Timestamp;
-				Items[ CurrentItem ].style.opacity = 1;
-				ChangeItem();
+				const Percentage = parseInt( Items[ CurrentItem ].style.width );
+				if ( Percentage >= 160 )
+				{
+					PreviousTimestamp = Timestamp;
+					CurrentSliderState = SliderStates.Fading;
+					CurrentItemWidth = OriginalItemWidth;
+					CurrentItemWidthSpeed = OriginalItemWidthSpeed;
+				}
+				else
+				{
+					Items[ CurrentItem ].style.width = `${ CurrentItemWidth }%`;
+					CurrentItemWidth += CurrentItemWidthSpeed;
+					CurrentItemWidthSpeed /= ItemWidthDeceleration;
+				}
 			}
+			break;
+
+			case ( SliderStates.Fading ):
+			{
+				const OpacityChange = ( Timestamp - PreviousTimestamp ) / ItemFadeSpeed;
+				console.log( Items[ CurrentItem ].style.opacity - OpacityChange );
+				Items[ CurrentItem ].style.opacity -= OpacityChange;
+				if ( Items[ CurrentItem ].style.opacity <= 0 )
+				{
+					CurrentSliderState = SliderStates.Zooming;
+					Items[ CurrentItem ].style.opacity = 1;
+					Items[ CurrentItem ].style.width = `${ OriginalItemWidth }%`;
+					ChangeItem();
+				}
+			}
+			break;
 		}
+
 		requestAnimationFrame( Update );
 	};
+
+	for ( i = 0; i < Items.length; i++ )
+	{
+		Items[ i ].style.opacity = 1;
+	}
 
 	SetImagePriority();
 	requestAnimationFrame( Update );
