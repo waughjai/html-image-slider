fbcba9cbc675457872dc0d4238aeb8cbdf944a70
jjwmezun <jjwmezun@gmail.com>
2018-12-27 16:39:51 -0800

    Resort files

--- demo.php
+++ demo.php
@@ -6,12 +6,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link href="index.css" rel="stylesheet" />
+<link href="css/slider.css" rel="stylesheet" />
 </head>
 <body>
 <?php
 	$sizes = [ [ 'w' => '500', 'h' => '334' ], [ 'w' => '1000', 'h' => '667' ], [ 'w' => '2000', 'h' => '1333' ], [ 'w' => '3000', 'h' => '2000' ] ];
-	$loader = new FileLoader([ 'directory-url' => 'http://localhost/slider', 'shared-directory' => 'img' ]);
+	$loader = new FileLoader([ 'directory-url' => 'http://localhost/slider', 'shared-directory' => 'demo-img' ]);
 	echo new HTMLImageSlider
 	(
 		[
@@ -23,6 +23,6 @@
 		$loader
 	);
 ?>
-<script src="index.js"></script>
+<script src="js/slider.js"></script>
 </body>
 </html>
